70c9c1731d4ced661ea44a32788b2637b4ab197a
ChinmayShukla04 <chinmayshukla04@gmail.com>
2026-04-12 12:17:03 +0530

    "updated the authentication file"

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,22 +1,49 @@
-import axios from "axios";
+// import axios from "axios";
 
-const API = "https://credoai-backend.onrender.com/";
+// const API = "https://credoai-backend.onrender.com";
+
+// const authService = {
+//   login: (email, password) =>
+//     axios.post(`${API}/auth/login`, { email, password }),
+
+//   register: (data) =>
+//     axios.post(`${API}/auth/register`, data),
+
+//   verifyOtp: (email, otp) =>
+//     axios.post(`${API}/auth/verify-otp`, { email, otp }),
+
+//   forgotPassword: (email) =>
+//     axios.post(`${API}/auth/forgot-password`, { email }),
+
+//   resetPassword: (email, otp, new_password) =>
+//     axios.post(`${API}/auth/reset-password`, {
+//       email,
+//       otp,
+//       new_password,
+//     }),
+// };
+
+// export default authService;
+
+
+
+import api from "./api";
 
 const authService = {
   login: (email, password) =>
-    axios.post(`${API}/auth/login`, { email, password }),
+    api.post("/auth/login", { email, password }),
 
   register: (data) =>
-    axios.post(`${API}/auth/register`, data),
+    api.post("/auth/register", data),
 
   verifyOtp: (email, otp) =>
-    axios.post(`${API}/auth/verify-otp`, { email, otp }),
+    api.post("/auth/verify-otp", { email, otp }),
 
   forgotPassword: (email) =>
-    axios.post(`${API}/auth/forgot-password`, { email }),
+    api.post("/auth/forgot-password", { email }),
 
   resetPassword: (email, otp, new_password) =>
-    axios.post(`${API}/auth/reset-password`, {
+    api.post("/auth/reset-password", {
       email,
       otp,
       new_password,
